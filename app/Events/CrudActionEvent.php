6aa58d11b88cfac4002afb15dd75a0d1e60c54f9
Boniface Muganda <bonifacemuganda@gmail.com>
2025-08-17 10:10:34 +0300

    First Commit

---
+++ app/Events/CrudActionEvent.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CrudActionEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct( public $instance, public $entry )
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel( 'channel-name' ),
+        ];
+    }
+}
